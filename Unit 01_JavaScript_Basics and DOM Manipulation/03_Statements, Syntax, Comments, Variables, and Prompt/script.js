498cbb74fe70799fdc6d824db18b5dc49ea8371d
Clarence Martin <marticla000@warren.k12.in.us>
2024-01-10 09:08:25 -0500

    initiate commit

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/03_Statements, Syntax, Comments, Variables, and Prompt/script.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Enter your name");
+// console.log(firstname);
+//YOUDO: The same for last name
+let fullName = firstName + lastName;
+console.log(fullName);
+document.getElementById("name").innerHTML + fullName;
