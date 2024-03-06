aa9a7a5fa3255f490c5637d7494f459f9ed9bf82
Clarence Martin <marticla000@warren.k12.in.us>
2024-03-06 08:58:10 -0500

    initiate commit

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -7,4 +7,12 @@ function takeSquareRoot() {
     
 }
 
+function takeAbsoluteValue() {
+    let number = +document.getElementById("input1").value;
+
+    let absolutevalue = Math.abs(number);
+    document.getElementById("result").innerHTML = absolutevalue;
+
+}
+
 // Make another function for takeAbsoluteValue
